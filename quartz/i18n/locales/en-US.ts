fbbf9c7e3a33919b8cdf6c0b296f206f8d035750
Luca Ramseyer <luca.ramseyer@lupree.com>
2024-05-31 13:54:18 +0200

    Quartz sync: May 31, 2024, 1:54 PM

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -33,7 +33,7 @@ export default {
       title: "Explorer",
     },
     footer: {
-      createdWith: "Created with",
+      createdWith: "Luca RAmseyer",
     },
     graph: {
       title: "Graph View",
